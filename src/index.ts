c3c8f23967ab909dabd24659d15b4a13b6e48b24
RdotSilva <RyandotSilva@gmail.com>
2020-07-22 19:11:37 -0400

    Add TODO to add sorting implementation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 class Sorter {
   constructor(public collection: number[]) {}
+
+  sort(): void {
+    // TODO: Add bubble sorting implementation
+  }
 }
 
 const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
