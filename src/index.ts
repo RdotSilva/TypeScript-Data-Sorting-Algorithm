8f1004d689663234ccec1876823bc2aab31971f6
RdotSilva <RyandotSilva@gmail.com>
2020-07-23 19:37:03 -0400

    Add nested for loops for sort

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ class Sorter {
   constructor(public collection: number[]) {}
 
   sort(): void {
-    // TODO: Add bubble sorting implementation
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {}
+    }
   }
 }
 
