85482a62f5910a8304226071ef07c6f78681627c
RdotSilva <RyandotSilva@gmail.com>
2020-07-23 19:42:16 -0400

    Add bubble sort logic to sort method

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 class Sorter {
   constructor(public collection: number[]) {}
 
+  /*
+   * Sorting algorithm using Bubble Sort
+   */
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {}
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
     }
   }
 }
