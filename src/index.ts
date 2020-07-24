ea4520e5a496717362a25e5fbaadc876966b9e0c
RdotSilva <RyandotSilva@gmail.com>
2020-07-23 20:59:28 -0400

    Add TODO to implement string sorting logic

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ class Sorter {
   sort(): void {
     const { length } = this.collection;
 
+    // TODO: Add implementation that will handle sorting of strings. As it currently stands the algorithm will not work for strings. Only an array.
+
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
         if (this.collection[j] > this.collection[j + 1]) {
