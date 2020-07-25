e03bf1c9440463c637cac641d50cf06278ba76db
RdotSilva <RyandotSilva@gmail.com>
2020-07-24 20:32:42 -0400

    Add type guard for Array

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,13 @@ class Sorter {
       for (let j = 0; j < length - i - 1; j++) {
         // All of this only works if collection nis number[]
         // If collection is an array of numbers then run this
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
         }
 
         // Will only work if collection is a string
