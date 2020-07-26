7d678a046a6f438f6099ca354a8ed0809f8ea385
RdotSilva <RyandotSilva@gmail.com>
2020-07-26 13:43:03 -0400

    Add Sortable interface

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,6 +1,10 @@
-import { NumbersCollection } from "./NumbersCollection";
+interface Sortable {
+  length: number;
+  compare(leftIndex: number, rightIndex: number): boolean;
+  swap(leftIndex: number, rightIndex: number): void;
+}
 export class Sorter {
-  constructor(public collection: NumbersCollection) {}
+  constructor(public collection: Sortable) {}
 
   /*
    * Sorting algorithm using Bubble Sort
