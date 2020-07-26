5c1477ac9c9cac721e6f2040866f6c0113aab233
RdotSilva <RyandotSilva@gmail.com>
2020-07-25 23:26:00 -0400

    Add compare method

--- src/NumbersCollection.ts
+++ src/NumbersCollection.ts
@@ -1,3 +1,7 @@
 export class NumbersCollection {
   constructor(public data: number[]) {}
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex] > this.data[rightIndex];
+  }
 }
