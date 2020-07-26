5fa7341e73c114b1d7c50da7d85c3c7beae9e570
RdotSilva <RyandotSilva@gmail.com>
2020-07-26 13:02:47 -0400

    Refactor Sorter class using NumbersCollection

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,5 +1,6 @@
+import { NumbersCollection } from "./NumbersCollection";
 export class Sorter {
-  constructor(public collection: ) {}
+  constructor(public collection: NumbersCollection) {}
 
   /*
    * Sorting algorithm using Bubble Sort
@@ -9,11 +10,8 @@ export class Sorter {
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
-        }
+        if (this.collection.compare(j, j + 1)) {
+          this.collection.swap(j, j + 1);
       }
     }
   }
