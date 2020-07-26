c54f23cfaa1a41bef78d39a534978a87b9f9bae1
RdotSilva <RyandotSilva@gmail.com>
2020-07-25 23:28:36 -0400

    Add swap method

--- src/NumbersCollection.ts
+++ src/NumbersCollection.ts
@@ -4,4 +4,10 @@ export class NumbersCollection {
   compare(leftIndex: number, rightIndex: number): boolean {
     return this.data[leftIndex] > this.data[rightIndex];
   }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftHand = this.data[leftIndex];
+    this.data[leftIndex] = this.data[rightIndex];
+    this.data[rightIndex] = leftHand;
+  }
 }
