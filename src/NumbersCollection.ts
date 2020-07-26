76431aab0f2e4ce202cdeaa6d095e09f5ff46987
RdotSilva <RyandotSilva@gmail.com>
2020-07-26 12:54:42 -0400

    Add length getter

--- src/NumbersCollection.ts
+++ src/NumbersCollection.ts
@@ -1,6 +1,10 @@
 export class NumbersCollection {
   constructor(public data: number[]) {}
 
+  get length(): number {
+    return this.data.length;
+  }
+
   compare(leftIndex: number, rightIndex: number): boolean {
     return this.data[leftIndex] > this.data[rightIndex];
   }
