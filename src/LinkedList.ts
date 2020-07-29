01f33445aea788ba4a96c5c31ead6fd18ae1bf1b
RdotSilva <RyandotSilva@gmail.com>
2020-07-28 20:27:22 -0400

    Add add method

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -6,4 +6,21 @@ class Node {
 
 export class LinkedList {
   head: Node | null = null;
+
+  add(data: number): void {
+    const node = new Node(data);
+
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let tail = this.head;
+
+    while (tail.next) {
+      tail = tail.next;
+    }
+
+    tail.next = node;
+  }
 }
