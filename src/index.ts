d23e5d8af8f3f9305d113fc5f45c4276464a4f86
RdotSilva <RyandotSilva@gmail.com>
2020-07-29 20:51:50 -0400

    Add test case for linked list sorting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
-import { CharactersCollection } from "./CharactersCollection";
+import { CharactersCollection } from "./CharactersCollection";}
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([-2, 10000, 3, -5, 0]);
 const numberSorter = new Sorter(numbersCollection);
@@ -8,8 +9,17 @@ const numberSorter = new Sorter(numbersCollection);
 const charactersCollection = new CharactersCollection("XaaybCsZv");
 const stringSorter = new Sorter(charactersCollection);
 
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+const linkedListSorter = new Sorter(linkedList);
+
 numberSorter.sort();
 stringSorter.sort();
+linkedListSorter.sort();
 
 console.log(numbersCollection.data);
 console.log(charactersCollection.data);
+linkedList.print();
