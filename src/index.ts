cbef92c98e84d59ec64c15973fcb3e5a56377dcc
RdotSilva <RyandotSilva@gmail.com>
2020-07-30 20:27:58 -0400

    Add new test case for number sorting

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,21 @@ import { CharactersCollection } from "./CharactersCollection";}
 import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([-2, 10000, 3, -5, 0]);
-const numberSorter = new Sorter(numbersCollection);
-
-const charactersCollection = new CharactersCollection("XaaybCsZv");
-const stringSorter = new Sorter(charactersCollection);
-
-const linkedList = new LinkedList();
-linkedList.add(500);
-linkedList.add(-10);
-linkedList.add(-3);
-linkedList.add(4);
-const linkedListSorter = new Sorter(linkedList);
-
-numberSorter.sort();
-stringSorter.sort();
-linkedListSorter.sort();
-
+numbersCollection.sort();
 console.log(numbersCollection.data);
-console.log(charactersCollection.data);
-linkedList.print();
+
+// const charactersCollection = new CharactersCollection("XaaybCsZv");
+// const stringSorter = new Sorter(charactersCollection);
+
+// const linkedList = new LinkedList();
+// linkedList.add(500);
+// linkedList.add(-10);
+// linkedList.add(-3);
+// linkedList.add(4);
+// const linkedListSorter = new Sorter(linkedList);
+
+// stringSorter.sort();
+// linkedListSorter.sort();
+
+// console.log(charactersCollection.data);
+// linkedList.print();
