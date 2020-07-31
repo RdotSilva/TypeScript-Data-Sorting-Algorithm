debbb83a95a4bd4a98d9df83f378ca494a7f3632
RdotSilva <RyandotSilva@gmail.com>
2020-07-31 18:20:33 -0400

    Remove unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
-import { CharactersCollection } from "./CharactersCollection";}
-import { LinkedList } from './LinkedList';
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
+// NumbersCollection test
 const numbersCollection = new NumbersCollection([-2, 10000, 3, -5, 0]);
 numbersCollection.sort();
 console.log(numbersCollection.data);
 
+// CharactersCollection test
 const charactersCollection = new CharactersCollection("XaaybCsZv");
 charactersCollection.sort();
 console.log(charactersCollection.data);
 
+// LinkedList test
 const linkedList = new LinkedList();
 linkedList.add(500);
 linkedList.add(-10);
