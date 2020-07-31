3d4504a875855bac816c55b74b8543a9837fc45d
RdotSilva <RyandotSilva@gmail.com>
2020-07-31 18:19:33 -0400

    Add test case for linked list sorting

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,11 @@ console.log(numbersCollection.data);
 const charactersCollection = new CharactersCollection("XaaybCsZv");
 charactersCollection.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
+linkedList.print();
