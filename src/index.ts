0bdfbb93b82dfc410f05b97a78c83acb083bb90e
RdotSilva <RyandotSilva@gmail.com>
2020-07-31 18:18:28 -0400

    Add test case for string sorting

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,6 @@ const numbersCollection = new NumbersCollection([-2, 10000, 3, -5, 0]);
 numbersCollection.sort();
 console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection("XaaybCsZv");
-// const stringSorter = new Sorter(charactersCollection);
-
-// const linkedList = new LinkedList();
-// linkedList.add(500);
-// linkedList.add(-10);
-// linkedList.add(-3);
-// linkedList.add(4);
-// const linkedListSorter = new Sorter(linkedList);
-
-// stringSorter.sort();
-// linkedListSorter.sort();
-
-// console.log(charactersCollection.data);
-// linkedList.print();
+const charactersCollection = new CharactersCollection("XaaybCsZv");
+charactersCollection.sort();
+console.log(charactersCollection.data);
