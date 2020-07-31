e1d29596c8ef08adb4d33bcb5f369ac2d6adf47a
RdotSilva <RyandotSilva@gmail.com>
2020-07-30 20:15:37 -0400

    Remove constructor

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -4,18 +4,16 @@ interface Sortable {
   swap(leftIndex: number, rightIndex: number): void;
 }
 export class Sorter {
-  constructor(public collection: Sortable) {}
-
   /*
    * Sorting algorithm using Bubble Sort
    */
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
       }
     }
   }
