d982da5106108935534b8fdae2b3b000ffb32488
RdotSilva <RyandotSilva@gmail.com>
2020-08-02 11:13:48 -0400

    Add unit test for add method

--- tests/LinkedListTests.ts
+++ tests/LinkedListTests.ts
@@ -12,4 +12,11 @@ describe("Linked List Tests", () => {
     linkedList.sort();
     expect(linkedList.at(0).data).to.equal(-10);
   });
+
+  it("add method should add node to linked list", () => {
+    const linkedList = new LinkedList();
+    linkedList.add(500);
+    expect(linkedList.at(0).data).to.equal(500);
+    expect(linkedList.length).to.equal(1);
+  });
 });
